a1061680baa081771202ea4c679962e57f4217b3
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-23 20:19:45 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
   def keys_of(arguments)
     # code goes here
+    arr = []
+    if arguments == "Panama"
+      arr = ["red-footed tortoise"]
+    elsif arguments == "Madagascar"
+      arr = ["aye-aye", "tomato frog"]
+    elsif arguments == "Australia"
+      arr = ["sugar glider", "kangaroo", "koala"]
+    end
+    
+    arr
   end
 end
